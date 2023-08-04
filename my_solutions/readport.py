15a32663543fe037d511bf20b7ae63992b824adc
meetagni <meetagnihotri@gmail.com>
2023-08-04 14:08:13 +0530

    2.2

--- my_solutions/readport.py
+++ my_solutions/readport.py
@@ -3,4 +3,15 @@ import csv
 
 #a function that reads a fileinrto a list of dicts
 def read_portfolio(filename):
-    
+    portfolio= []
+    with open(filename) as f:
+        rows= csv.reader(f)
+        header= next(rows)
+        for row in rows:
+            record={
+                'name': row[0],
+                'shares': int(row[1]),
+                'price': float(row[2])
+            }
+            portfolio.append(record)
+    return portfolio
